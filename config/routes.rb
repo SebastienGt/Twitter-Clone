87bbc8c495c5873635e1c17de3abf7c49fb8f178
SebastienGt <sebastien.friedberg@gmail.com>
2022-02-11 16:22:35 +0100

    Boostrap initialize

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'static_pages#home'
+  get 'static_pages/about'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
