c311f89a63928438fca6ef489cf18faa464af1b3
SebastienGt <sebastien.friedberg@gmail.com>
2022-02-16 13:16:39 +0100

    Implement log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   	def create
 		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		if user&.authenticate(params[:session][:password])
 			# login and redirect to the page
 			reset_session
 			log_in user
@@ -18,5 +18,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		log_out
+		redirect_to root_url
 	end
 end
