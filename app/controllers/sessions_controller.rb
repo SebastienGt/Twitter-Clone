777c46cc3f33bc48d92752aca918084961e8949e
SebastienGt <sebastien.friedberg@gmail.com>
2022-02-16 14:37:16 +0100

    Impement remember me login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,10 @@ class SessionsController < ApplicationController
 
   	def create
 		user = User.find_by(email: params[:session][:email].downcase)
-		if user&.authenticate(params[:session][:password])
+		if user && user.authenticate(params[:session][:password])
 			# login and redirect to the page
 			reset_session
+			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 			log_in user
 			redirect_to user
 		else
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		log_out
+		log_out if logged_in?
 		redirect_to root_url
 	end
 end
