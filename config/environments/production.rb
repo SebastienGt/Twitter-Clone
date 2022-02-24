874b894ad3a16451e73dfd793f4ed21c1b28d01f
SebastienGt <sebastien.friedberg@gmail.com>
2022-02-24 12:59:19 +0100

    Adding comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -128,7 +128,7 @@ Rails.application.configure do
     :authentification => :plain,
     :user_name => 'apikey',
     :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'heroku.com'
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
 
