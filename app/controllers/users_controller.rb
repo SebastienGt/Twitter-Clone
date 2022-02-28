fe592789cd169dfa878d7a2aadff87eabd54dc16
SebastienGt <sebastien.friedberg@gmail.com>
2022-02-28 13:09:37 +0100

    Removing sending mail for prod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			@user.send_activation_email
+			#@user.send_activation_email
 			flash[:info] = "Please check your email account to activate your account."
 			flash[:success] = "Welcome to the app !"
 			#redirect_to root_url
