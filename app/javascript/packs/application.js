215b037e44598eb4de0109c87346e15115f73864
SebastienGt <sebastien.friedberg@gmail.com>
2022-03-01 14:03:07 +0100

    Adding React components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,12 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+
+// Seb
+window.jQuery = $;
+window.$ = $;
+
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
